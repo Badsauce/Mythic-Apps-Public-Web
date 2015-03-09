e11abfd51fe400583e975d87e62e3e5f8d1b75fe
Badsauce <Ninjazoby@gmail.com>
2015-03-09 17:39:05 -0400

    added the widget

--- js/mailjet-widget.js
+++ js/mailjet-widget.js
@@ -1,7 +1,7 @@
 $( document ).ready(function() {
   $("#mailjet-widget").on("submit", function (e) {
     e.preventDefault();
-    var url = "addContact?"+$("#mailjet-widget").serialize();
+    var url = "http://mailapi.mythicapps.io/addContact?"+$("#mailjet-widget").serialize();
     $.post( url, function( data ) {
       if (data.status === "ok"){
         alert(data.message);
