e204615fe52a433c44be0444269a1a7bcfeea876
Badsauce <Ninjazoby@gmail.com>
2015-03-09 23:24:39 -0400

    Happy mail widget has decent usability

--- js/mailjet-widget.js
+++ js/mailjet-widget.js
@@ -3,12 +3,17 @@ $( document ).ready(function() {
     e.preventDefault();
     var url = "http://mailapi.mythicapps.io/addContact?"+$("#mailjet-widget").serialize();
     $.post( url, function( data ) {
+      $(".loader").hide();
       if (data.status === "ok"){
-        alert(data.message);
+        $(".mailjet-widget-status").addClass("success").text(data.message).show();
       }
       else if (data.status === "error"){
-        alert(data.message);
+        $("#mailjet-widget").css('animation','none').toggle("slow");
+        $(".mailjet-widget-status").addClass("error").text(data.message).show();
       }
     });
+    $(".mailjet-widget-status").attr("class","mailjet-widget-status").hide();
+    $("#mailjet-widget").toggle("slow");
+    $(".loader").show();
   });
 });
