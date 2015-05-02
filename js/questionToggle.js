6302f172dd4ecca5d7a34cd69d149ff3e9da2483
Badsauce <Ninjazoby@gmail.com>
2015-05-01 22:26:29 -0400

    bells and whistles, macbook bro media queries

--- js/questionToggle.js
+++ js/questionToggle.js
@@ -1,22 +1,31 @@
 $( document ).ready(function() {
   $('.button--faq').click(function(e){
     e.preventDefault();
-    $('.questions').removeClass('selected');
     switch(e.target.id) {
       case 'general-button':
+        $('.questions').removeClass('selected');
+        $('.button--faq').removeClass('selected');
+        $(e.target).addClass('selected');
         $('#general-questions').addClass('selected');
         break;
       case 'before-button':
+        $('.questions').removeClass('selected');
+        $('.button--faq').removeClass('selected');
+        $(e.target).addClass('selected');
         $('#before-questions').addClass('selected');
         break;
       case 'during-button':
+        $('.questions').removeClass('selected');
+        $('.button--faq').removeClass('selected');
+        $(e.target).addClass('selected');
         $('#during-questions').addClass('selected');
         break;
       case 'after-button':
+        $('.questions').removeClass('selected');
+        $('.button--faq').removeClass('selected');
+        $(e.target).addClass('selected');
         $('#after-questions').addClass('selected');
         break;
-      default:
-        alert('unsupported id '+e.target.id);
     }
   })
 });
