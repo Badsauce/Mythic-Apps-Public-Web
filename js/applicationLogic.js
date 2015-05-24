326d235c9c5ba64b5318399d1e1f722fb10aa995
badsauce <ninjazoby@gmail.com>
2015-05-24 17:23:26 -0500

    added logic for revealing applications

--- js/applicationLogic.js
+++ js/applicationLogic.js
@@ -1,7 +1,15 @@
 $( document ).ready(function() {
-  //Bind a on click event to apply button
-
-  //Toggle the application section to visible
-
-  //move view to application
+  //if we add more events escape them into a function "attachEvents"
+  $('#apply').click(revealApplication);
 });
+
+function revealApplication(){
+  $('#application').slideDown();
+  scrollTo("#application", 1000);
+}
+
+function scrollTo(selector, animationTime) {
+  $('html, body').animate({
+        scrollTop: $(selector).offset().top
+    }, animationTime);
+}
